1a1f9137e89291d03f31c12001aafa25d62b5cf7
volllga <volllga@ya.ru>
2022-04-03 08:57:37 +0300

    project Virtual Piano is created

---
+++ Virtual Piano/task/src/eventHandler.js
@@ -0,0 +1,30 @@
+document.addEventListener("keydown", function(event) {
+    if (event.code === "AltRight") {
+        console.log(event, event.code);
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "W") {
+        console.log("W Pressed", 'event.key = ', event.key, 'event.code = ', event.code);
+    }
+});
+
+document.addEventListener("keydown", function(e) {
+    if (e.code === "Digit2") {
+        console.log(e);
+    }
+});
+
+document.addEventListener("keyup", function(event) {
+    if (event.code === "ControlLeft") {
+        console.log(event);
+        return event;
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "V") {
+        return event;
+    }
+});
